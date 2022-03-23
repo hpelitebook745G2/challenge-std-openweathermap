78dd318b0b08c3e8f1c8351741e926cc8bf0f1a3
Earl Grant Reyes <earl.reyes@activate.sg>
2022-03-24 00:39:52 +0800

    Updated Prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
   arrowParens: 'avoid',
 };
