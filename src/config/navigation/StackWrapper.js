c7a7f6ebe24cfd2d53f1c6426c61088048bcb2b9
Earl Grant Reyes <earl.reyes@activate.sg>
2022-03-24 00:47:28 +0800

    NavigationContainer

---
+++ src/config/navigation/StackWrapper.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as screens from '../../containers';
+
+const DashboardStack = createNativeStackNavigator();
+
+const App = () => (
+  <NavigationContainer>
+    <DashboardStack.Navigator>
+      <DashboardStack.Screen
+        name="MapScreen"
+        options={{ title: 'MyOpenWeatherMap' }}
+        component={screens.MapScreen}
+      />
+    </DashboardStack.Navigator>
+  </NavigationContainer>
+);
+
+export default App;
