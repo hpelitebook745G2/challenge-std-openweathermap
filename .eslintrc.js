d13948487cd972d7b6d4c05043a22f578c446f2e
Earl Grant Reyes <earl.reyes@activate.sg>
2022-03-25 17:39:32 +0800

    Disabled some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,10 @@ module.exports = {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'func-names': 0,
     'react/function-component-definition': 0,
+    'class-methods-use-this': 0,
+    'global-require': 0,
+    'react/no-unstable-nested-components': 0,
+    'react/jsx-props-no-spreading': 0,
+    'no-shadow': 0,
   },
 };
